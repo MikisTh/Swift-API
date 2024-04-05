dbf20106c54983b545d8f437c11911864c0b3cac
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-05 11:58:30 -0300

    Add
    add

---
+++ SWIFT/functions.swift
@@ -0,0 +1,19 @@
+// Para usar uma função, você “chama” essa função com seu nome e passa a ela valores de entrada (argumentos) sendo eles os parâmetros da função.
+
+func functionName(ParameterName: type) ->returnType{
+// Body of the function
+}
+func addFruits( string Fruit ) {
+    print("nameFruit")
+    return (fruit)
+}
+addFruits()
+
+func addVegetables( string Vegetable ) {
+    print("nameVegetable")
+    return (vegetable)
+}
+addVegetables()
+
+
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {}
