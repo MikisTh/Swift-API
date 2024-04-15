130d2446c8a2f72b2ec6941fd62c75d6514a2577
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-04-15 17:32:18 -0300

    Add
    Add

---
+++ SWIFT/arrays1.swift
@@ -0,0 +1,30 @@
+
+let fruits: [String] = ["Uva", "Maça", "Banana", "Mamão", "Laranja", "Morango", "Melancia", "Melão", "Abacaxi", "Abacate", "Jabuticaba", "Caju", "Cereja", "Kiwi", "Goiaba", "Cajá", "Acerola", "Maracujá", "Manga", "Pera"]
+
+fruits.append(6)
+fruits.sort()
+fruits.sort(by: >)
+fruits.shuffle()
+let shuffledFruits = fruits.shuffled()
+
+
+print("Tamanho do array: \(fruits.count)")
+
+
+for fruit in fruits {
+    print("Fruit: \(fruit)")
+}
+
+fruits.forEach { fruit in
+    print(fruit)
+}
+
+fruits.swapAt(1, 2)
+
+sort()	classifica elementos de matriz
+shuffle()	altera a ordem dos elementos de matriz
+forEach()	chama uma função para cada elemento
+contains()	Procura o elemento em uma matriz
+swapAt()	troca a posição dos elementos da matriz
+reverse()	inverte a ordem dos elementos de matriz
+
