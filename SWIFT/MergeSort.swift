473221d722195e96ef2d95e2ae28ba0b1a1defb8
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-19 11:35:21 -0300

    Update MergeSort.swift
    Add

--- SWIFT/MergeSort.swift
+++ SWIFT/MergeSort.swift
@@ -1,10 +1,47 @@
-func mergeSort<T:Comparable>(_ array:[T])->[T] {
-    
-    guard array.count > 1 else {
-        return array
+func mergeSort<T: Comparable>(_ array: [T]) -> [T] {
+  guard array.count > 1 else { return array }
+
+  let middleIndex = array.count / 2
+  
+  let leftArray = mergeSort(Array(array[0..<middleIndex]))
+  let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+  
+  return merge(leftArray, rightArray)
+}
+
+func merge<T: Comparable>(_ left: [T], _ right: [T]) -> [T] {
+  var leftIndex = 0
+  var rightIndex = 0
+
+  var orderedArray: [T] = []
+  
+  while leftIndex < left.count && rightIndex < right.count {
+    let leftElement = left[leftIndex]
+    let rightElement = right[rightIndex]
+
+    if leftElement < rightElement {
+      orderedArray.append(leftElement)
+      leftIndex += 1
+    } else if leftElement > rightElement {
+      orderedArray.append(rightElement)
+      rightIndex += 1
+    } else { 
+      orderedArray.append(leftElement)
+      leftIndex += 1
+      orderedArray.append(rightElement)
+      rightIndex += 1
     }
-    let left = Array(array[0..<array.count/2])
-    let right = Array(array[array.count/2..<array.count])
- 
-    return mergeArray(left: mergeSort(left), right: mergeSort(right))
+  }
+
+  while leftIndex < left.count {
+    orderedArray.append(left[leftIndex])
+    leftIndex += 1
+  }
+
+  while rightIndex < right.count {
+    orderedArray.append(right[rightIndex])
+    rightIndex += 1
+  }
+  
+  return orderedArray
 }
