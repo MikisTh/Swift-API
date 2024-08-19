dbee1887a6e16637629b5245a1a2dff392620f16
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-19 10:35:55 -0300

    Update SelectSort.swift
    Add

--- SWIFT/SelectSort.swift
+++ SWIFT/SelectSort.swift
@@ -2,11 +2,8 @@
  
 func selectionSortA(arr: [Int]) -> [Int] {
 
-   // Creating a copy of original array
    var nArr = arr 
     
-   // Selecting minimum element and 
-   // then place it at the beginning of the array
    for x in 0..<nArr.count {
       var mIndex = x
       for y in x+1..<nArr.count {
@@ -23,4 +20,4 @@ func selectionSortA(arr: [Int]) -> [Int] {
 
 let array = [9, 1, 8, 1, 3, 5, 10, 2, 8, 23, 6]
 let resultantArr = selectionSortAlgo(arr: array)
-print("Sorted array in ascending
+print("Sorted array in ascending")
