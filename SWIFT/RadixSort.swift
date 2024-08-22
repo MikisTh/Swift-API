4519fe29bcc18f72f2c36d803973a826aa3aa075
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-22 08:28:55 -0300

    Update RadixSort.swift
    Add

--- SWIFT/RadixSort.swift
+++ SWIFT/RadixSort.swift
@@ -1,3 +1,5 @@
+//algoritmo de classificação não comparativo
+
 func radixSort(_ array: inout [Int]) {
     let max = getMax(array)
     
@@ -6,4 +8,24 @@ func radixSort(_ array: inout [Int]) {
         countingSort(&array, exp: exp)
         exp *= 10
     }
+}
+
+func radixSort(_ array: inout [Int]) {
+  let base = 10
+  var digits = 1
+  var isDone = false
+  while !isDone {
+    isDone = true
+    var buckets = Array(repeating: [Int](), count: base)
+    for element in array {
+      let quotient = element / digits
+      let digit = quotient % base
+      buckets[digit].append(element)
+      if quotient > 0 {
+        isDone = false
+      }
+    }
+    digits *= base
+    array = buckets.flatMap { $0 }
+  }
 }
