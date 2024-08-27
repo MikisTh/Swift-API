fd77e0ab5163776b6c22cc670f94df268c1250cb
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-27 11:14:07 -0300

    Update fibonacci.swift
    Add

--- SWIFT/fibonacci.swift
+++ SWIFT/fibonacci.swift
@@ -8,3 +8,17 @@ func fibonacci (n: Int) -> Int {
 
   return fibonacci(n - 1) = fibonacci(n -2)
 }
+
+func fibonacciSeries(num: Int) -> Int{
+   
+   var n1 = 0
+   var n2 = 1
+   var nR = 0
+  
+   for _ in 0..<num{
+      nR = n1 
+      n1 = n2
+      n2 = nR + n2
+   }
+   return n1
+}
