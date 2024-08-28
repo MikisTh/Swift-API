07713d95fb0fbdba191c16dacc90b81cd2969726
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-28 10:37:32 -0300

    Create TacDri.swift
    Add

---
+++ Swift-Test/TacDri.swift
@@ -0,0 +1,168 @@
+
+var NEAT:Int!
+print(NEAT)
+if NEAT != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+var TEF:Int!
+print(TEF)
+if TEF != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+var AT:Int!
+print(AT)
+if AT != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+function imc(){
+  
+    let altura = prompt('Digite sua Altura:');
+    let peso = prompt('Digite seu peso:');  
+  
+    let imc = peso / (altura * altura)    
+
+    alert(`Seu IMC é ${imc}`);
+
+
+var IMC:Int!
+print(IMC)
+if IMC != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+var TMB:Int!
+var idade: int
+var sexo: string = feminino || masculino
+var peso: int
+
+ if idade = 0 || < 3 && sexo = feminino {
+	print ("(58,317 x peso em kg) – 31,1")
+} else if idade = 3 || idade < 10 && sexo = feminino {
+	print ("(20,315 x peso em kg) + 485,9") 
+} else if idade = 10 || idade < 18 && sexo = feminino {
+	print ("(13,384 x peso em kg) + 692,6") 
+} else if idade = 18 || idade < 30 && sexo = feminino {
+	print ("(14,818 x peso em kg) + 486,6")
+} else if idade = 30 || idade < 60 && sexo = feminino
+	print ("(8,126 x peso em kg) + 845,6")
+} else idade >= 60 && sexo = feminino 
+	print ("(9,082 x peso em kg) + 658,5") 
+
+ if idade = 0 || < 3 && sexo = masculino {
+	print ("(59,512 x peso em kg) – 30,4")
+} else if idade = 3 || idade < 10 && sexo = masculino {
+	print ("(22,706 x peso em kg) + 504,3") 
+} else if idade = 10 || idade < 18 && sexo = masculino {
+	print ("(17,686 x peso em kg) + 658,2") 
+} else if idade = 18 || idade < 30 && sexo = masculino {
+	print ("(15,057 x peso em kg) + 692,2")
+} else if idade = 30 || idade < 60 && sexo = masculino
+	print ("(11,472 x peso em kg) + 873,1")
+} else idade >= 60 && sexo = feminino 
+	print ("(11,711 x peso em kg) + 587,7")
+
+
+print(TMB)
+
+var FAT:Int!
+print(FAT)
+if IMC != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+Se você é Sedentário (pouco ou nenhum exercício)
+Calorias por dia = TMB x 1,2
+
+Se você é Levemente Ativo (exercícios leves ou pratica 1-3 dias / semana)
+Calorias por dia = TMB x 1.375
+
+Se você é Moderadamente Ativo (exercícios moderados de 3-5 dias / semana)
+Calorias por dia = TMB x 1,55
+
+Se você é Muito Ativo (exercício intenso de 6 a 7 dias / semana)
+Calorias por dia = TMB x 1.725
+
+Se você é Super Ativo (exercício muito pesado e trabalho labor físico / braçal)
+Calorias por dia = TMB x 1,9
+
+var MET:Int!
+Gasto energético = MET  x  peso corporal (kg)   x   tempo da atividade (h)
+print(MET)
+
+** // Valores de Referência do MET para Homens(70 kg)
+
+if MET < 4 {
+ 	print ("baixa intensidade")
+} else if MET = 4 || MET < 6 { 
+	print("intensidade média") 
+} else if MET = 6 || MET < 8 {
+	print ("intensidade média-alta")
+} else if MET = 8 || MET < 10 {
+	print("intensidade muito alta") 
+} else MET > 10 {
+ print ("intensidade extremamente alta")
+}
+
+** // Valores de Referência do MET para Mulheres(50 kg) 
+
+if MET < 2,8 {
+ 	print ("baixa intensidade")
+} else if MET = 2,8 || MET < 4,3 { 
+	print("intensidade média") 
+} else if MET = 4,4 || MET < 5,9 {
+	print ("intensidade média-alta")
+} else if MET = 6 || MET < 7,5 {
+	print("intensidade muito alta") 
+} else MET > 7,6 {
+ print ("intensidade extremamente alta")
+}
+
+var GET:Int!
+
+
+if idade = 18 || idade <= 30 && sexo = feminino {
+    	GET = 239 X (Peso [Kg] X 0,062 + 2,036) = GEB
+} else if idade = 18 || idade < 30 && sexo = masculino {
+	GET = 239 X (Peso [Kg] X 0,063 + 2,896) = GEB 
+} else if idade = 31 || idade = 40 && sexo = feminino {
+	GET = 239 X (Peso [Kg] X 0,034 + 3,538) = GEB
+} else idade = 31 || idade = 40 && sexo = masculino {
+ 	GET = 239 X (Peso [Kg] X 0,048 + 3,653) = GEB
+}
+
+print(GET)
+
+
+var FERRO:Int!
+print(FERRO)
+if IMC != nil {
+	print("It has some value \(someValue!)")
+} else {
+	print("doesn't contain value")
+}
+
+var balancoNitrogenado:Int!
+let balancoNitrogenado = nitrogenioIng - nitrogenioExc
+
+print(balancoNitrogenado)
+
+if nitrogenioIng > nitrogenioExc {
+	print("Balanço Nitrogenado Positivo, seu organismo está ganhando proteínas.")
+} else {
+	print("Balanço Nitrogenado Negativo, seu organismo está perdendo proteínas")
+}
+
+** 0,8 a 1,2 gramas de proteína por quilo de peso corporal por dia
