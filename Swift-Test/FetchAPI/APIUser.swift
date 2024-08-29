3284777907942048f3a8697fa796d483d9ca7023
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-08-29 10:06:49 -0300

    Create APIUser.swift
    Add

---
+++ Swift-Test/FetchAPI/APIUser.swift
@@ -0,0 +1,59 @@
+// APIClient.swift
+
+func fetchTaco(completion: @escaping (Result<[Taco], Error>) -> Void) {
+
+    guard let url = URL(string: "https://www.tbca.net.br/base-dados/busca_componente.php") else {
+        return 
+    }
+
+    URLSession.shared.dataTask(with: url) { data, response, error in
+
+        // Handle response here
+
+    }.resume()
+
+}
+
+func Taco(completion: @escaping (Result<[Taco], Error>) -> Void)) {
+
+    // Existing request logic
+
+    URLSession.shared.dataTask(with: url) { data, _, error in
+
+        guard let data = data else { 
+            return completion(.failure(error))  
+        }
+
+        let decoder = JSONDecoder()
+
+        do {
+            let Taco = try decoder.decode(Taco.self, from: data)
+            completion(.success(Taco.results)) 
+        } catch {
+            completion(.failure(error))
+        }
+
+    }.resume()   
+}
+
+func Taco() {
+
+    APIClient().fetchTaco { result in
+
+        switch result {
+
+            case .success(let films):
+
+                DispatchQueue.main.async {
+                    self.taco = films 
+                    self.tableView.reloadData()
+                }
+
+            case .failure(let error):
+                print(error)
+
+        }
+
+    }
+
+}
