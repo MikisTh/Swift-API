f3a40ca1f71d6d30cf9fe6f34de51bf253d1c49b
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-02 11:50:29 -0300

    Create getResquest.swift
    Add

---
+++ Swift-Test/FetchAPI/getResquest.swift
@@ -0,0 +1,55 @@
+
+func getRequest(url: String,
+                 completion: @escaping ([String: Any]?, Error?) -> Void){
+    //URL válida
+    guard let URL = URL(string: url) else {
+        completion(nil, nil)
+        return
+    }
+    
+    //Cria a representacão da requisição
+    let request = NSMutableURLRequest(url: URL)
+    
+    //Request method GET
+    request.httpMethod = "GET"
+      
+    //Cria a tarefa de requisição
+    let task = URLSession.shared.dataTask(with: request as URLRequest) {
+        (data, response, error) in
+        do {
+            
+            if let data = data {
+                // response sucess
+                let response = try JSONSerialization.jsonObject(with: data, options: [])
+                completion(response as? [String : Any], nil)
+            }
+            else {
+                //no response
+                completion(nil, nil)
+            }
+        } catch let error as NSError {
+            // error comunication server
+            completion(nil, error)
+        }
+    }
+    
+    
+//on task
+    task.resume()
+    
+}
+
+// URL API
+let url = "https://jsonplaceholder.typicode.com/todos/1"
+
+//Call Function Get
+getRequest(url: url){
+    (resultado, erro)  in
+      if(resultado != nil) {
+          //O resultado aqui vem como Opcional
+          print("Sua requisicao foi realizada com êxito! : \n \(resultado)")
+      } else {
+          print("A requisicao falhou!")
+      }
+      
+}
