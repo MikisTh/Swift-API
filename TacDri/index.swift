00bffd439723b859588189783d175d18ed86cb35
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-02 11:34:17 -0300

    Update index.swift
    Add

--- TacDri/index.swift
+++ TacDri/index.swift
@@ -4,3 +4,10 @@ let nome = "Usuário"
 let mensagem = saudacao + ", " + nome + "!""Seja bem vindo!"
 print(mensagem)
 
+//Função de Cálculo imc
+func imc(a: Int, b: Int) -> Int {
+    return a / b * b
+}
+
+let resultado = imc (a: 5, b: 3)
+print("O IMC é \(resultado)")
