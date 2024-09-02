8c467f05b58b1c04df14fe70b52dd6ddd5265094
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-02 11:43:37 -0300

    Update postRequest.swift
    Add

--- Swift-Test/FetchAPI/postRequest.swift
+++ Swift-Test/FetchAPI/postRequest.swift
@@ -1,9 +1,69 @@
+import.foundation
+
+func postRequest(url: String, params: [String: String], 
+                 completion: @escaping ([String: Any]?, Error?) -> Void){
+    //URL válida
+    guard let URL = URL(string: url) else {
+        completion(nil, nil)
+        return
+    }
+        
+    //Cria a representacão da requisição
+    let request = NSMutableURLRequest(url: URL)
+
+    //Converte as chaves em valores pares para os parametros em formato de String
+    let postString = params.map { "\($0.0)=\($0.1)" }.joined(separator: "&")
+
+    /Post Request Method
+    request.httpMethod = "POST"
+
+    //Codifica o corpo da mensagem em "data" usando utf8
+    request.httpBody = postString.data(using: String.Encoding.utf8)
+
+
+    //new task request
+    let task = URLSession.shared.dataTask(with: request as URLRequest) {
+        (data, response, error) in
+        do {
+
+            if let data = data {
+                //response
+                let response = try JSONSerialization.jsonObject(with: data, options: [])
+                completion(response as? [String : Any], nil)
+            }
+            else {
+                //not response
+                completion(nil, nil)
+            }
+        } catch let error as NSError {
+            //error
+            completion(nil, error)
+        }
+    }
+
+//Task on
+    task.resume()
+}
+
+//URL API
+let url = "http://www.SuaURL.com"
+
+//Params requisition
+let params = [
+            "nome":"Thiago Mikis",
+            "sobreNome":"Mikis",
+        ]
+
+//Call function POST
 postRequest(url: url, params: params){
                     (result, err)  in
+                    //Result
                     if let res:Bool = (result?.values.first as? Bool) {
-                        if(res) {                           
+                        if(res) {
+                            //true and false
                             print("Sua requisicao foi realizada com êxito!")
                         } else {
+                            //error
                             print("Sua requisicao falhou!")
                         }
                     }
