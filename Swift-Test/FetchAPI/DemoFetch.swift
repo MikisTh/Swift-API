4e6deb03980f47e4f4297741d6bd0e9f80f0df85
MikisTh <114635629+MikisTh@users.noreply.github.com>
2024-09-03 23:58:56 -0300

    Atualizar o DemoFetch.swift

--- Swift-Test/FetchAPI/DemoFetch.swift
+++ Swift-Test/FetchAPI/DemoFetch.swift
@@ -9,13 +9,13 @@ let task = URLSession.shared.dataTask(with: request){ data, response, error in
     }
 
 do {
-        let posts = try JSONDecoder().decode([Post].self, from: data) // Since the JSON in the URL starts with array([]), we will be using [Post].self. If the JSON starts with curly braces ({}), use Post.self
+        let posts = try JSONDecoder().decode([Post].self, from: data) 
         
         for post in posts {
             print("User ID:", post.userId)
             print("Id:", post.id)
-            print("Title:", post.title)
-            print("Body:", post.body)
+            print("nome:":", post.nome)
+            print("imc:", post.imc)
         }
     } catch let jsonError {
         print("Failed to decode json", jsonError)
